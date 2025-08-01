ed92c34331c6064feb949c58bf58be3591637d86
Lelo Dumeko <lelodumeko2@gmail.com>
2025-08-01 13:59:01 +0200

    2025.07.31FormComponent

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { SharedSoriesService } from '../../services/shared/shared-sories.service';
+import { StoryRecord } from '../../models/story.model';
+import { Router, ActivatedRoute  } from '@angular/router';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css',
+})
+export class FormComponent {
+  currentRecord: StoryRecord = {
+    title: '',
+    plot: '',
+  };
+  
+  constructor(private sharedServ: SharedSoriesService, private router: Router, private route: ActivatedRoute) {}
+
+  private isFormValid(): boolean {
+    return (
+      this.currentRecord.title.trim() !== '' &&
+      this.currentRecord.plot.trim() !== '' && 
+      this.currentRecord.plot.trim().length <= 1000
+    );
+  }
+
+  saveStory() {
+    if (this.isFormValid()) {
+      this.sharedServ.createStory(this.currentRecord);
+      console.log('Successfully saved record');
+    } else {
+      console.error('Invalid form submission');
+      return;
+    }
+
+    this.resetForm();
+    this.goBack();
+  }
+
+  resetForm() {
+    this.currentRecord = {
+      title: '',
+      plot: '',
+    };
+  }
+
+  goBack() {
+   const character = this.route.parent?.snapshot.params['character'] || 'mina';
+    this.router.navigate(['/log', character]);
+  }
+}
